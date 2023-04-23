d07a227d0e9c2776ba0f68afc6d23d74c1fb4493
jubrealguy <whizkidjubrealguy@gmail.com>
2023-04-23 22:31:59 +0100

    connect without password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,11 +3,11 @@
 file { '/etc/ssh/ssh_config':
   ensure => present,
 }
--> file_line { 'change pass id to /etc/ssh/ssh_config':
+file_line { 'changed access permission from yes to no':
   path => '/etc/ssh/ssh_config',
   line => '    PasswordAuthentication no',
 }
-file_line { 'change IdentutyFile to /etc/ssh/ssh_config':
+file_line { 'changed the private key':
   path => '/etc/ssh/ssh_config',
   line => '    IdentityFile ~/.ssh/school',
 }
