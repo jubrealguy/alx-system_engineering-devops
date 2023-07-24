a97491ce1d20f9f0d45b4e994288afbe20a15243
jubrealguy <whizkidjubrealguy@gmail.com>
2023-07-24 20:43:43 +0100

    changed todod to todos

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,8 +6,8 @@ from sys import argv
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(argv[1])).json()
-    todos = requests.get(url + "todod", params={"userId": argv[1]}).json()
+    todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
-    completed =[todo["title"] for todo in todos if todo["completed"] == True]
+    completed =[todo["title"] for todo in todos if todo["completed"]]
     print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(comp)) for comp in completed]
