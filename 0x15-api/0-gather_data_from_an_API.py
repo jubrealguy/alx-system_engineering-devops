e29936e917890a102541886861b596fdcf14befb
jubrealguy <whizkidjubrealguy@gmail.com>
2023-07-24 20:31:35 +0100

    gather todo

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that returns the todo list of a given employee ID"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todos = requests.get(url + "todod", params={"userId": argv[1]}).json()
+
+    completed =[todo["title"] for todo in todos if todo["completed"] == True]
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(comp)) for comp in completed]
