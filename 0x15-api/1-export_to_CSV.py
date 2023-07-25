b813306b4795458e4df5e471a7be3417ed8b3b51
jubrealguy <whizkidjubrealguy@gmail.com>
2023-07-25 12:03:36 +0100

    arranged imported libraries alphabetically

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """A script that exports the todo list of a given employee ID to CSV"""
+import csv
 import requests
 from sys import argv
-import csv
 
 if __name__ == "__main__":
     USER_ID = argv[1]
